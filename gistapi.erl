2e6dd54703656e68e73f8f36074f88c60ee34e7c
Daniel Schauenberg <d@unwiredcouch.com>
2010-06-13 02:39:50 +0200

    basic gist fetching working

--- gistapi.erl
+++ gistapi.erl
@@ -1,18 +1,45 @@
 -module(gistapi).
 -export([get_gist/1]).
 
-% BASE URL definitions
+% defines
 -define(BASE_API_URL, "http://gist.github.com/api/v1/").
 -define(BASE_RAW_URL, "http://gist.github.com/raw/").
+-define(GITHUB, "github.com").
+
+% functions to make the actual requests
+
+get_gist(ID) ->
+  Url = get_gist_url(yaml, ID),
+  Response = http_get(Url),
+  Response.
+
+% helper functions
+http_get(Url) ->
+  inets:start(),
+  case http:request(get, {Url, []}, [], []) of
+        {ok, {{_, 200, _}, _, Body}} ->
+          Response = Body;
+        {ok, {{_, 403, _}, _, _}} ->
+          Response = forbidden;
+        {ok, {{_, 404, _}, _, _}} ->
+          Response = not_found;
+        {ok, _} ->
+          Response = request_failed;
+        {error, Reason} ->
+          io:format("timeout? ~p~n", [Reason]),
+          Response = Reason
+  end,
+  inets:stop(),
+  Response.
 
 % functions to get the assembled URLs
 get_gist_url(yaml, ID) ->
-  ?BASE_API_URL ++ yaml ++ "/" ++ ID.
+  ?BASE_API_URL ++ "yaml/" ++ ID.
 
 get_users_gists_url(yaml, Login) ->
-  ?BASE_API_URL ++ yaml ++ "/gists/" ++ Login.
+  ?BASE_API_URL ++ "yaml/gists/" ++ Login.
 
 get_gist_content_url(ID, File) ->
   ?BASE_RAW_URL ++ ID ++ "/" ++ File.
 
-% functions to make the actual requests
+
